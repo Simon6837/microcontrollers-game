7a4f34401dd64a7efd8d621ba43ae42c3b157608
kvincn <115541436+kvincn@users.noreply.github.com>
2023-11-20 14:08:23 +0100

    ir-send + recieve basics done

--- src/IR.cpp
+++ src/IR.cpp
@@ -1,5 +1,6 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <HardwareSerial.h>
 
 void IR_innit(){ //initializes the infraredcommunication
 // Timer0_innit();
@@ -14,16 +15,15 @@ void Timer0_innit(){ //initializes timer0 for ir communication
         CS0[2:0] = 0b001 (1/8 prescaler)
 
         (OCR0A + 1) * N / F_CPU = 1/36000 s
-        OCR0A = 1/36000 * (16000000/8) - 1 = 54.5555556 
+        OCR0A = 1/38000 * (16000000/8) - 1 = 54.5555556 
         OCR0A on 55 gives around 35714 hz
     */
 
    TCCR0A |= (1 << WGM01);
-   TCCR0B |= (1 << CS01);
-   OCR0A = 52;
+   TCCR0B |= (1 << CS00);
+   OCR0A = 211;
    TCNT0 = 0;
    TIMSK0 |= (1 << OCIE0A);
-
 
 }
 ISR (TIMER0_COMPA_vect){
@@ -31,10 +31,10 @@ ISR (TIMER0_COMPA_vect){
 }
 
 int main(){
-    // Timer0_innit();
+    Timer0_innit();
     DDRD |= (1 << DDD6);
-    DDRD |= (1 << DDD2);
     PORTD |= (1 << PORTD6);
+    Serial.begin(9600);
     sei();
     while (1)
     {
