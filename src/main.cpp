d25c2b4a37f89c3057fae7c970c1efb936dfa577
Simon van den Berg <simonschrotenberg@gmail.com>
2023-11-20 13:09:52 +0100

    added base code for screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,29 @@
-#include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_ILI9341.h>
+#include <Adafruit_STMPE610.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define TFT_CS 10
+#define TFT_DC 9
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+
+
+void setup(void)
+{
+  Serial.begin(9600);
+  tft.begin();
+
+  tft.fillScreen(ILI9341_BLACK);
+  // origin = left,top landscape (USB left upper)
+  tft.setRotation(1); 
+tft.println("Hello World!");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+
 }
+
+
+
