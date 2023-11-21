dba4a56562821b512404e1c0267ec059a65ab3b3
JunctionParadox <novasigma89@gmail.com>
2023-11-21 12:37:04 +0100

    Added basic movement functionality

--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,55 @@
 #include <Wire.h>
 #include <Adafruit_ILI9341.h>
 #include <Adafruit_STMPE610.h>
+#include <classes/player.h>
 
+#define KEY_UP 72
+#define KEY_DOWN 80
+#define KEY_LEFT 75
+#define KEY_RIGHT 77
 #define TFT_CS 10
 #define TFT_DC 9
+volatile int c;
+volatile int x;
+volatile int y;
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
+void draw_screen() {
+  tft.fillScreen(ILI9341_BLACK);
+}
+
+void draw_player() {
+  tft.fillCircle(x, y, 20, ILI9341_WHITE);
+}
+
+void player_start() {
+  x = 120;
+  y = 160;
+  //Player player_ship(120, 160);
+}
+
+//Something that manipulates x and y based on nunchuck movement
+void player_move() {
+  y++;
+}
 
 void setup(void)
 {
   Serial.begin(9600);
   tft.begin();
+  player_start();
 
   tft.fillScreen(ILI9341_BLACK);
   // origin = left,top landscape (USB left upper)
-  tft.setRotation(1); 
-tft.println("Hello World!");
+  tft.setRotation(2);
+//tft.println("Hello World!");
 }
 
 void loop()
 {
-
+  draw_screen();
+  draw_player();
+  player_move();
 }
 
 
