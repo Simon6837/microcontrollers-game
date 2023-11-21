8548efe0945082362371b1c27fbce5aee97704b7
JunctionParadox <novasigma89@gmail.com>
2023-11-21 12:37:22 +0100

    Added incomplete class functions

---
+++ src/classes/player.cpp
@@ -0,0 +1,12 @@
+// #include "player.h"
+
+// Player::Player()
+// {
+//     this->x = x;
+//     this->y = y;
+// };
+
+// void Player::draw_player()
+// {
+//     tft.fillCircle(x, y, 20, ILI9341_BLACK);
+// }
---
+++ src/classes/player.h
@@ -0,0 +1,15 @@
+// #ifndef PLAYER_H
+// #define PLAYER_H
+
+// class Player
+// {
+//     public:
+//         Player(uint8_t x, uint16_t y);
+//         void draw_player();
+
+//     protected:
+//         uint8_t x;
+//         uint16_t y;
+// };
+
+// #endif
