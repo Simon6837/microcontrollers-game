1231b755ee9d7e797013a73f134efa80fec59e20
kvincn <115541436+kvincn@users.noreply.github.com>
2023-12-07 13:18:40 +0100

    movement

--- src/main.cpp
+++ src/main.cpp
@@ -15,15 +15,23 @@
 // pins for the screen
 #define TFT_CS 10 // Chip select line for TFT display
 #define TFT_DC 9  // Data/command line for TFT
+
+const uint16_t onesecondcount = 38000;
 // setup needed objects
 Adafruit_ILI9341 LCD = Adafruit_ILI9341(TFT_CS, TFT_DC);
 NunchukController nunchukController;
 BulletList bulletList;
 Player player(120, 280, &LCD, &nunchukController, &bulletList);
-Enemy enemy(30, 35, &LCD, 1);
-Enemy enemies[4][5]{{enemy, enemy, enemy, enemy, enemy}, {enemy, enemy, enemy, enemy, enemy}, {enemy, enemy, enemy, enemy, enemy}, {enemy, enemy, enemy, enemy, enemy}};
 IR ir_comm;
+Enemy *enemy0 = new Enemy(30, 35, &LCD, 0);
+Enemy *enemy1 = new Enemy(30, 35, &LCD, 1);
+Enemy *enemies[4][5]{{enemy1, enemy1, enemy1, enemy1, enemy1},
+                     {enemy0, enemy0, enemy0, enemy0, enemy0},
+                     {enemy0, enemy0, enemy0, enemy0, enemy0},
+                     {enemy0, enemy0, enemy0, enemy0, enemy0}};
 
+uint16_t counteronesec = 0;
+uint8_t timemovement = 0;
 /**
  * @brief Sets up the screen and the player than connects to the Nunchuk
  */
@@ -33,12 +41,11 @@ void setup()
   LCD.begin();
   LCD.fillScreen(ILI9341_BLACK);
   LCD.setRotation(2);
-  enemies[2][3].killEnemy();
-  for (uint8_t j = 0; j < 4; j++) //voor rijen links en rechts j/2 als rest 1 = links als rest = 0 rechts
+  for (uint8_t j = 0; j < 4; j++) // voor rijen links en rechts j/2 als rest 1 = links als rest = 0 rechts
   {
     for (uint8_t i = 0; i < 5; i++)
     {
-      enemies[j][i].drawEnemy((i * 40), j * 50); //voor rijen links en rechts j/2 als rest 1 = tijdsverplaatsing + als rest = 0 tijdsverplaatsing -, als max reset tijdsverplaatsing
+      enemies[j][i]->drawEnemy((i * 40), (j * 50) + (1 * timemovement)); // voor rijen links en rechts j/2 als rest 1 = tijdsverplaatsing + als rest = 0 tijdsverplaatsing -, als max reset tijdsverplaatsing
     }
   }
   player.drawPlayer();
@@ -49,6 +56,28 @@ void setup()
 ISR(TIMER0_COMPA_vect)
 {
   PORTD ^= (1 << PORTD6);
+  counteronesec++;
+  if (counteronesec == onesecondcount){
+    if (timemovement < 4)
+    {timemovement++;
+      for (uint8_t j = 0; j < 4; j++) // voor rijen links en rechts j/2 als rest 1 = links als rest = 0 rechts
+  {
+    for (uint8_t i = 0; i < 5; i++)
+    {
+      enemies[j][i]->drawEnemy((i * 40) + (4 * timemovement), (j * 50)); // voor rijen links en rechts j/2 als rest 1 = tijdsverplaatsing + als rest = 0 tijdsverplaatsing -, als max reset tijdsverplaatsing
+    }
+  }}
+    else 
+    {timemovement = 0;
+          for (uint8_t j = 0; j < 4; j++) // voor rijen links en rechts j/2 als rest 1 = links als rest = 0 rechts
+  {
+    for (uint8_t i = 0; i < 5; i++)
+    {
+      enemies[j][i]->drawEnemy((i * 40)  + (1 * timemovement), (j * 50)); // voor rijen links en rechts j/2 als rest 1 = tijdsverplaatsing + als rest = 0 tijdsverplaatsing -, als max reset tijdsverplaatsing
+    }
+  }}
+    counteronesec = 0;
+  }
 }
 
 int main(void)
