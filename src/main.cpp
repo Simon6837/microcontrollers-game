8cfa86ad8838aa2e6a7b5b8768f2f2fe2e7b2145
JunctionParadox <novasigma89@gmail.com>
2023-12-21 14:20:46 +0100

    Added menu functions

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,8 @@ Adafruit_ILI9341 LCD = Adafruit_ILI9341(TFT_CS, TFT_DC);
 NunchukController nunchukController;
 // varibles needed for the game
 bool playerIsMoving = false;
+volatile uint8_t gameState = 0;
+volatile int8_t menuState = 0;
 // enemies array, initialized here to prevent stack overflow
 Enemy enemies[4][5] = {
     {Enemy(30, 35, &LCD, 1), Enemy(30, 35, &LCD, 2), Enemy(30, 35, &LCD, 3), Enemy(30, 35, &LCD, 4), Enemy(30, 35, &LCD, 1)},
@@ -110,21 +112,110 @@ ISR(TIMER2_OVF_vect)
 
 ISR(TIMER1_COMPA_vect)
 {
-  bulletList.updateBullets();
-  counteronesec++;
-  if (counteronesec == 45) // TODO: remove magic number (could be made dynamic to increase difficulty)
-  {
-    Enemy::moveEnemy(enemies, timemovement, maxTimeMovement);
-    timemovement++;
-    if (timemovement == maxTimeMovement)
+    bulletList.updateBullets();
+    counteronesec++;
+    if (counteronesec == 45) // TODO: remove magic number (could be made dynamic to increase difficulty)
     {
-      timemovement = 0;
+      Enemy::moveEnemy(enemies, timemovement, maxTimeMovement);
+      timemovement++;
+      if (timemovement == maxTimeMovement)
+      {
+        timemovement = 0;
+      }
+      redrawEnemy = true;
+      counteronesec = 0;
     }
-    redrawEnemy = true;
-    counteronesec = 0;
-  }
 }
 
+void showMenu() {
+  gameState = 0;
+  //stop timer 1
+  TIMSK1 &= ~(1<<OCIE1A);
+  //title
+  LCD.drawChar(40, 40, 'S', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  LCD.drawChar(52, 40, 'P', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  LCD.drawChar(64, 40, 'A', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  LCD.drawChar(76, 40, 'C', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  LCD.drawChar(88, 40, 'E', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  LCD.drawChar(120, 40, 'D', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  LCD.drawChar(132, 40, 'E', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  LCD.drawChar(144, 40, 'F', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  LCD.drawChar(156, 40, 'E', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  LCD.drawChar(168, 40, 'N', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  LCD.drawChar(180, 40, 'D', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  LCD.drawChar(192, 40, 'E', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  LCD.drawChar(204, 40, 'R', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  //Single player option
+  LCD.drawRect(64, 100, 112, 40, ILI9341_WHITE);
+  LCD.drawChar(76, 115, 'S', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  LCD.drawChar(88, 115, 'I', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  LCD.drawChar(100, 115, 'N', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  LCD.drawChar(112, 115, 'G', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  LCD.drawChar(124, 115, 'L', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  LCD.drawChar(136, 115, 'E', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  //Versus
+  LCD.drawRect(64, 160, 112, 40, ILI9341_WHITE);
+  LCD.drawChar(76, 175, 'V', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  LCD.drawChar(88, 175, 'E', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  LCD.drawChar(100, 175, 'R', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  LCD.drawChar(112, 175, 'S', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  LCD.drawChar(124, 175, 'U', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  LCD.drawChar(136, 175, 'S', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  //Coop
+  LCD.drawRect(64, 220, 112, 40, ILI9341_WHITE);
+  LCD.drawChar(76, 235, 'C', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  LCD.drawChar(88, 235, 'O', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  LCD.drawChar(100, 235, '-', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  LCD.drawChar(112, 235, 'O', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+  LCD.drawChar(124, 235, 'P', ILI9341_WHITE, ILI9341_WHITE, 2, 2);
+}
+
+void flickerButton() {
+  LCD.drawRect(64, 220, 112, 40, ILI9341_RED);
+  LCD.drawRect(64, 220, 112, 40, ILI9341_WHITE);
+  LCD.drawRect(64, 220, 112, 40, ILI9341_RED);
+  LCD.drawRect(64, 220, 112, 40, ILI9341_WHITE);
+  LCD.drawRect(64, 220, 112, 40, ILI9341_RED);
+}
+
+void menuControlsEnable() {
+    if (menuState < 0) {
+      menuState = 0;
+    }
+    else if (menuState > 47) {
+      menuState = 47;
+    }
+    if (nunchukController.updateMenu()==0) {
+      menuState++;
+    }
+    if (nunchukController.updateMenu()==1) {
+      menuState--;
+    }
+    if (menuState < 16) {
+      LCD.drawRect(64, 100, 112, 40, ILI9341_YELLOW);
+      LCD.drawRect(64, 160, 112, 40, ILI9341_WHITE);
+      LCD.drawRect(64, 220, 112, 40, ILI9341_WHITE);
+      if (nunchukController.isZButtonPressed()==true) {
+        gameState = 1;
+        TIMSK1 |= (1 << OCIE1A);
+        LCD.fillScreen(ILI9341_BLACK);
+        player.drawPlayer();
+      }
+    }
+    if (menuState > 15 && menuState < 32) {
+      LCD.drawRect(64, 100, 112, 40, ILI9341_WHITE);
+      LCD.drawRect(64, 160, 112, 40, ILI9341_GREEN);
+      LCD.drawRect(64, 220, 112, 40, ILI9341_WHITE);
+    }
+    if (menuState > 31) {
+      LCD.drawRect(64, 100, 112, 40, ILI9341_WHITE);
+      LCD.drawRect(64, 160, 112, 40, ILI9341_WHITE);
+      LCD.drawRect(64, 220, 112, 40, ILI9341_YELLOW);
+      if (nunchukController.isZButtonPressed()==true) {
+        flickerButton();
+      }
+    }
+}
 /**
  * @brief Sets up the screen and the player than connects to the Nunchuk
  * Also initializes the timers and the ADC.
@@ -141,7 +232,7 @@ void setup()
   LCD.begin();
   LCD.fillScreen(ILI9341_BLACK);
   LCD.setRotation(2);
-  player.drawPlayer();
+  showMenu();
   nunchukController.initialize();
   ir_comm.IR_innit();
   player.displayLives();
@@ -166,7 +257,12 @@ int main(void)
   setup();
   while (1)
   {
-    player.controlPlayer();
+    if (gameState == 0) {
+      menuControlsEnable();
+    }
+    if (gameState == 1) {
+      player.controlPlayer();
+    }
   }
   return 0;
 }
